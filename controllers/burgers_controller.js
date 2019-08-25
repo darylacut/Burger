1fe7ea7303915638859822884d4dab4ff38d7d41
Daryl Acut <darylacut@yahoo.com>
2019-08-25 05:46:57 -0400

    created controller.js

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,56 @@
+var express = require('express');
+
+var burger = require("../models/burger.js");
+
+var router = express.Router();
+
+router.get("/", function(req, res) {
+    burger.selectAll(function(data) {
+        var bgObject = {
+            burgers: data
+        };
+        console.log(bgObject);
+        res.render("index", bgObject);
+    });
+});
+
+router.post("/api/burgers", function(req, res) {
+    burger.insertOne([
+        "name", "devoured"
+    ], [
+        req.body.name, req.body.devoured
+    ], function(result) {
+        res.json({ id: result.insertId });
+    });
+});
+
+router.put("api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+    burger.updateOne({
+        devoured: req.body.devoured 
+        }, 
+        condition, function(result) {
+            if (result.changeRows == 0) {
+                return res.status(404).end();
+            } else {
+                res.status(200).end();
+            }
+        });
+    });
+
+// router.delete("api/burgers/:id", function(req, res) {
+//     var condition = "id = " + req.params.id;
+
+//     burger.delete(condition, function(req, res) {
+//         if (result.affectedRows == 0) {
+//             return (res.status(404).end();
+//         } else {
+//             res.status(200).end();
+//         }
+//     });
+});
+
+module.exports = router;
